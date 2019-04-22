4d3448e29dbea3b3c3f8bc0f070f7d961115ff55
Leonardo Varini <leonardo.varini@selbetti.com>
2019-04-22 16:04:14 -0300

    Conexão com Banco de Dados

---
+++ GerenciamentoContatos.Database/DbConn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace GerenciamentoContatos.Database
+{
+    public class DbConn : Smart.Database.DbConn
+    {
+        public override string BuildConnString()
+        {
+            this.ProviderName = "System.Data.SqlClient";
+            return ConfigurationManager.AppSettings["ConnectionString"];
+            
+        }
+    }
+}
