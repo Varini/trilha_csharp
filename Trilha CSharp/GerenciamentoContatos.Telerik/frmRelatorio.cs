69b2f7eabfc9ca0dc64266d11229564f59350a29
Leonardo Varini <leonardo.varini@selbetti.com>
2019-05-06 16:35:47 -0300

    Recuperação Telerik

---
+++ Trilha CSharp/GerenciamentoContatos.Telerik/frmRelatorio.cs
@@ -0,0 +1,21 @@
+﻿
+namespace GerenciamentoContatos
+{
+    public partial class frmRelatorio : Telerik.WinControls.UI.RadForm
+    {
+        public frmRelatorio()
+        {
+            InitializeComponent();
+        }
+
+        public void CarregarRelatorio(Telerik.Reporting.Report rpt) 
+        {
+            rpvContatos.ReportSource = rpt;            
+
+            rpvContatos.Show();
+
+            rpvContatos.RefreshReport();        
+        }
+
+    }
+}
